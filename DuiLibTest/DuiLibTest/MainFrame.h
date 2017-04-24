80f48d345358f856863ff66aae12ac898ab06a11
hewei <hewei0810@qq.com>
2017-04-24 17:29:33 +0800

    add WndUI

--- DuiLibTest/DuiLibTest/MainFrame.h
+++ DuiLibTest/DuiLibTest/MainFrame.h
@@ -2,6 +2,7 @@
 #define __MAIN_FRAME_H__
 
 #include "UITimeScaleBar.h"
+#include "UIWnd.h"
 
 class CMainFrame : public WindowImplBase
 {
@@ -22,6 +23,12 @@ public:
 	virtual CControlUI* CreateControl(LPCTSTR pstrClass){
 		if (_tcscmp(pstrClass, _T("TimeScaleBar")) == 0)
 			return new CTimeScaleBarUI;
+		else if (_tcscmp(pstrClass, _T("Wnd")) == 0){
+			CWndUI* pWndUI = new CWndUI;
+			HWND    hWnd  = CreateWindow(_T("static"), _T("win32"), WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, m_PaintManager.GetPaintWindow(), NULL, NULL, NULL);
+			pWndUI->Attach(hWnd);
+			return pWndUI;
+		}
 		return NULL;
 	}
 	virtual void OnFinalMessage(HWND hWnd){
